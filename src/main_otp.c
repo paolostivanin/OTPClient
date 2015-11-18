11c1bbdbba4f59007fb1842cbdcbe5f5101591bf
Paolo Stivanin <info@paolostivanin.com>
2015-11-18 21:22:44 +0100

    working on the codebase

--- src/main_otp.c
+++ src/main_otp.c
@@ -39,7 +39,7 @@ main (void)
     
     // TODO: read password from user
     // TODO: gcry_malloc_secure pwd user
-    char *sec_key = decrypt_skey (enc_key);
+    char *sec_key = decrypt_skey (user_pwd, enc_key);
     // TODO: zeroing out pwd
     // TODO: zeroing out sec_key
     
@@ -53,7 +53,7 @@ main (void)
 
 
 char
-*read_file(char *account_name)
+*read_file (char *account_name)
 {
     size_t i;
     char *ac_nm;
@@ -75,19 +75,20 @@ char
         if (strcmp (ac_nm, account_name) == 0)
         {
             enc_key_tk = strtok (NULL, " ");
-            e_key = malloc (strlen (enc_key_tk)+1);
+            e_key = gcry_malloc_secure (strlen (enc_key_tk)+1);
             if (e_key == NULL)
             {
                 fprintf (stderr, "[!] ERROR during memory allocation\n");
                 free (line);
                 return NULL;
             }
-            for (i=0; i<strlen(enc_key_tk); i++)
+            for (i = 0; i < strlen (enc_key_tk); i++)
             {
                 if (enc_key_tk[i] == '\n')
                     enc_key_tk[i] = '\0';
             }
             strncpy (e_key, enc_key_tk, strlen (enc_key_tk));
+            memset (enc_key_tk, 0, sizeof (enc_key_tk));
             free (line);
             return e_key;
         }
@@ -98,12 +99,74 @@ char
 
 
 char
-*decrypt_skey (const char *pwd, const char *e_key)
+*encrypt_token (const char *pwd, char *plain_token)
 {
-    char *dec_key;
+    int algo = gcry_cipher_map_name ("aes256");
+    size_t blkLength = gcry_cipher_get_algo_blklen (algo);
+	size_t keyLength = gcry_cipher_get_algo_keylen (algo);
     
+    unsigned char *salt;
+    unsigned char *iv;
+    unsigned char *derived_crypto_key;
     
-    return dec_key;
+    salt = gcry_malloc (SALT_LEN);
+    if (salt == NULL)
+    {
+        return NULL;
+    }
+    
+    iv = gcry_malloc (blkLength);
+    if (iv == NULL)
+    {
+        gcry_free (salt);
+        return NULL;
+    }
+    
+    gcry_create_nonce (salt, SALT_LEN);
+    gcry_create_nonce (iv, blkLength);
+
+    gcry_cipher_hd_t hd;
+    gcry_cipher_open(&hd, algo, GCRY_CIPHER_MODE_CTR, 0);
+	if (((derived_crypto_key = gcry_malloc_secure (keyLength)) == NULL))
+    {
+		fprintf (stderr, "encrypt_file: memory allocation error\n");
+        gcry_free (salt);
+        gcry_free (iv);
+		return NULL;
+	}
+
+	if (gcry_kdf_derive (pwd, strlen (pwd), GCRY_KDF_PBKDF2, GCRY_MD_SHA512, salt, SALT_LEN, 50000, keyLength, derived_crypto_key) != 0)
+    {
+		fprintf (stderr, "encrypt_token: key derivation error\n");
+        gcry_free (salt);
+        gcry_free (iv);
+        gcry_free (derived_crypto_key);
+		return NULL;
+	}
+    
+	gcry_cipher_setkey (hd, derived_crypto_key, keyLength);
+    gcry_cipher_setctr (hd, iv, blkLength);
+    
+    gcry_cipher_encrypt (hd, ...);
+       
+    /* 1) encrypt e_key
+     * 2) concat (salt,iv,enc)
+     * 3) b64(4)
+     */
+     
+     gcry_free (salt);
+     gcry_free (iv);
+     gcry_free (derived_crypto_key);
+     
+     return b64_enc_key
 }
 
 
+char
+*decrypt_skey (const char *pwd, const char *e_key)
+{
+    /* 1) b64 decode e_key
+     * 2) split key (salt+iv+key)
+     * 3) use pwd to decrypt e_key
+     */ 
+}
--- src/otpclient.h
+++ src/otpclient.h
@@ -2,8 +2,10 @@
 #define OTPCLIENT_H_INCLUDED
 
 #define FILE_PATH ""
+#define SALT_LEN 32
 
 char *read_file (char *);
 char *decrypt_skey (const char *, const char *);
+char *encrypt_skey (const char *, const char *);
 
 #endif
