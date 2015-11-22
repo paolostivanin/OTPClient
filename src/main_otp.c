80f70543faf4a5ff57842a616127ac95ca26e061
Paolo Stivanin <info@paolostivanin.com>
2015-11-22 09:38:09 +0100

    enc and dec are now ready

--- src/main_otp.c
+++ src/main_otp.c
@@ -9,9 +9,23 @@
 #include "otpclient.h"
 
 
+struct _b64OutData {
+    unsigned char *out;
+    size_t out_len;
+};
+
+
 int
 main (void)
 {
+    if (!gcry_check_version ("1.5.0"))
+    {
+        printf ("libgcrypt required version is 1.5.0 and above\n");
+        return -2;
+    }
+    gcry_control (GCRYCTL_INIT_SECMEM, 16384, 0);
+    gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
+    
     size_t i;
     char *account_name_from_user;
     char buffer[128];
@@ -154,7 +168,7 @@ char
         multi_free (salt, iv, derived_crypto_key);
         return NULL;
     }
-    gcry_cipher_encrypt (hd, out_enc_text, strlen (plain_token), plain_token, strlen (plain_token)); //check if CTR MODE out_enc_len is equal to in_enc_len
+    gcry_cipher_encrypt (hd, out_enc_text, strlen (plain_token), plain_token, strlen (plain_token));
     
     concat_salt_iv_enc = gcry_malloc (SALT_LEN + blkLength + strlen (plain_token));
     if (concat_salt_iv_enc == NULL)
@@ -178,13 +192,91 @@ char
 
 
 char
-*decrypt_skey (const char *pwd, const char *e_key)
+*decrypt_token (const char *pwd, const char *encoded_key)
 {
-    /* 1) b64 decode e_key
-     * 2) split key (salt+iv+key)
-     * 3) use pwd to decrypt e_key
-     */
-    return dec_key;
+    int algo = gcry_cipher_map_name ("aes256");
+    size_t blkLength = gcry_cipher_get_algo_blklen (algo);
+	size_t keyLength = gcry_cipher_get_algo_keylen (algo);
+    
+    unsigned char *salt, *iv, *derived_crypto_key;
+    unsigned char *enc_text;
+    char *decrypted_plain_token;
+    
+    struct _b64OutData *OutData = b64_decode (encoded_key);
+    
+    salt = gcry_malloc (SALT_LEN);
+    if (salt == NULL)
+    {
+        free (OutData);
+        return NULL;
+    }
+    
+    iv = gcry_malloc (blkLength);
+    if (iv == NULL)
+    {
+        gcry_free (salt);
+        free (OutData);
+        return NULL;
+    }
+    
+    enc_text = gcry_malloc (OutData->enc_len);
+    if (enc_text == NULL)
+    {
+        multi_free (salt, iv, NULL);
+        free (OutData);
+        return NULL;
+    }
+    
+    memcpy (salt, OutData->out, SALT_LEN);
+    memcpy (iv, OutData->out + SALT_LEN, blkLength);
+    memcpy (enc_text, OutData->out + SALT_LEN + blkLength, OutData->enc_len);
+    
+    gcry_cipher_hd_t hd;
+    gcry_cipher_open(&hd, algo, GCRY_CIPHER_MODE_CTR, 0);
+	if (((derived_crypto_key = gcry_malloc_secure (keyLength)) == NULL))
+    {
+		fprintf (stderr, "encrypt_file: memory allocation error\n");
+        multi_free (salt, iv, enc_text);
+        free (OutData);
+		return NULL;
+	}
+
+	if (gcry_kdf_derive (pwd, strlen (pwd), GCRY_KDF_PBKDF2, GCRY_MD_SHA512, salt, SALT_LEN, 50000, keyLength, derived_crypto_key) != 0)
+    {
+		fprintf (stderr, "encrypt_token: key derivation error\n");
+        multi_free (salt, iv, derived_crypto_key);
+        gcry_free (enc_text);
+        free (OutData);
+		return NULL;
+	}
+    
+	gcry_cipher_setkey (hd, derived_crypto_key, keyLength);
+    gcry_cipher_setctr (hd, iv, blkLength);
+    
+    decrypted_plain_token = gcry_malloc (OutData->enc_len);
+    if (decrypted_token == NULL)
+    {
+        multi_free (salt, iv, derived_crypto_key);
+        gcry_free (enc_text);
+        free (OutData);
+		return NULL;
+    }
+    gcry_cipher_decrypt (hd, decrypted_plain_token, OutData->enc_len, enc_text, OutData->enc_len);
+    
+    if (!g_str_is_ascii (decrypted_plain_token))
+    {
+        printf ("[E] Decryption failed (wrong password)\n");
+        multi_free (salt, iv, derived_crypto_key);
+        gcry_free (enc_text);
+        free (OutData);
+        return NULL;
+    }
+    
+    multi_free (salt, iv, derived_key);
+    gcry_free (enc_text);
+    free (OutData);
+    
+    return decrypted_plain_token; // FREE AFTER USE
 }
 
 
@@ -192,15 +284,23 @@ char
 *b64_encode (unsigned char *in, size_t in_len)
 {
     size_t encoded_text_len = (((in_len/3)+1)*4)+4;
-    size_t out_len;
-    int state = 0, save = 0;
 
     char *out = gcry_malloc (encoded_text_len);
 
-    out_len = g_base64_encode_step (in, in_len, FALSE, out, &state, &save);
-    g_base64_encode_close (FALSE, out+out_len, &state, &save);
+    out = g_base64_encode (in, in_len);
 
     return out;
+}
+
+
+struct _b64OutData
+*b64_decode (const char *in)
+{
+    struct _b64OutData *OutData = (struct _b64OutData *) malloc (sizeof (struct _b64OutData));
+
+    OutData->out = g_base64_decode (in, &(OutData->out_len));
+
+    return OutData;
 }
 
 
