03649636c3bc25725915c47d3c61f97f015b3d41
Paolo Stivanin <info@paolostivanin.com>
2015-11-22 16:16:12 +0100

    codebase ready, will start gui

--- src/main_otp.c
+++ src/main_otp.c
@@ -5,13 +5,13 @@
 #include <gcrypt.h>
 #include <ctype.h>
 #include <glib.h>
-#include <cotp.h>
+//#include <cotp.h>
 #include "otpclient.h"
 
 
 struct _b64OutData {
     unsigned char *out;
-    size_t out_len;
+    size_t enc_len;
 };
 
 
@@ -26,49 +26,18 @@ main (void)
     gcry_control (GCRYCTL_INIT_SECMEM, 16384, 0);
     gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
     
-    size_t i;
-    char *account_name_from_user;
-    char buffer[128];
-    char *user_pwd;
     
-    printf ("Account name: ");
-    fgets (buffer, sizeof (buffer), stdin);
-    account_name_from_user = malloc (strlen (buffer)+1);
-    if (account_name_from_user == NULL)
-    {
-        fprintf (stderr, "[!] ERROR during memory allocation\n");
-        return -1;
-    }
-    strncpy (account_name_from_user, buffer, strlen(buffer)-1); // -1 because we delete the \n char
-    for (i=0; i<strlen(account_name_from_user); i++)
-        account_name_from_user[i] = tolower(account_name_from_user[i]);
-    
-    printf ("%s\n", account_name_from_user);
-    char *enc_key = read_file (account_name_from_user);
-    if (enc_key == NULL)
-    {
-        fprintf (stderr, "[!] ERROR something went wrong\n");
-        free (account_name_from_user);
-        return -1;
-    }
-    
-    // TODO: read password from user
-    // TODO: gcry_malloc_secure pwd user
-    char *sec_key = decrypt_skey (user_pwd, enc_key);
-    // TODO: zeroing out pwd
-    // TODO: zeroing out sec_key
-    
-    free (enc_key);
-    gcry_free (user_pwd);
-    gcry_free (sec_key);
-    free (account_name_from_user);
-    
+    /* 1) callback add_account (gcry_free (encrypt_token(pwd, plain_token)));
+     * 2) callback delete_account ();
+     * 3) callback view_token (gcry_free (decrypt_token(pwd, encoded_token)));
+     */
+   
     return 0;
 }
 
 
 char
-*read_file (const char *account_name)
+*read_file (const char *account_name, const char *file_path)
 {
     size_t i;
     char *ac_nm;
@@ -77,8 +46,8 @@ char
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
-
-    FILE *fp = fopen (FILE_PATH, "r");
+    
+    FILE *fp = fopen (file_path, "r");
     if (fp == NULL)
     {
         fprintf (stderr, "[!] ERROR opening file %s\n", FILE_PATH);
@@ -103,7 +72,7 @@ char
                     enc_key_tk[i] = '\0';
             }
             strncpy (e_key, enc_key_tk, strlen (enc_key_tk));
-            memset (enc_key_tk, 0, sizeof (enc_key_tk));
+            memset (enc_key_tk, 0, strlen (enc_key_tk));
             free (line);
             return e_key;
         }
@@ -219,7 +188,7 @@ char
         return NULL;
     }
     
-    enc_text = gcry_malloc (OutData->enc_len);
+    enc_text = gcry_malloc (OutData->enc_len-SALT_LEN-blkLength+1);
     if (enc_text == NULL)
     {
         multi_free (salt, iv, NULL);
@@ -229,7 +198,7 @@ char
     
     memcpy (salt, OutData->out, SALT_LEN);
     memcpy (iv, OutData->out + SALT_LEN, blkLength);
-    memcpy (enc_text, OutData->out + SALT_LEN + blkLength, OutData->enc_len);
+    memcpy (enc_text, OutData->out + SALT_LEN + blkLength, OutData->enc_len-SALT_LEN-blkLength);
     
     gcry_cipher_hd_t hd;
     gcry_cipher_open(&hd, algo, GCRY_CIPHER_MODE_CTR, 0);
@@ -253,15 +222,17 @@ char
 	gcry_cipher_setkey (hd, derived_crypto_key, keyLength);
     gcry_cipher_setctr (hd, iv, blkLength);
     
-    decrypted_plain_token = gcry_malloc (OutData->enc_len);
-    if (decrypted_token == NULL)
+    decrypted_plain_token = gcry_malloc_secure (OutData->enc_len-SALT_LEN-blkLength+1);
+    if (decrypted_plain_token == NULL)
     {
         multi_free (salt, iv, derived_crypto_key);
         gcry_free (enc_text);
         free (OutData);
 		return NULL;
     }
-    gcry_cipher_decrypt (hd, decrypted_plain_token, OutData->enc_len, enc_text, OutData->enc_len);
+    
+    gcry_cipher_decrypt (hd, decrypted_plain_token, OutData->enc_len, enc_text, OutData->enc_len-SALT_LEN-blkLength);
+    decrypted_plain_token[OutData->enc_len-SALT_LEN-blkLength] = '\0';
     
     if (!g_str_is_ascii (decrypted_plain_token))
     {
@@ -272,7 +243,7 @@ char
         return NULL;
     }
     
-    multi_free (salt, iv, derived_key);
+    multi_free (salt, iv, derived_crypto_key);
     gcry_free (enc_text);
     free (OutData);
     
@@ -298,7 +269,7 @@ struct _b64OutData
 {
     struct _b64OutData *OutData = (struct _b64OutData *) malloc (sizeof (struct _b64OutData));
 
-    OutData->out = g_base64_decode (in, &(OutData->out_len));
+    OutData->out = g_base64_decode (in, &(OutData->enc_len));
 
     return OutData;
 }
--- src/otpclient.h
+++ src/otpclient.h
@@ -4,8 +4,12 @@
 #define FILE_PATH ""
 #define SALT_LEN 32
 
-char *read_file (const char *);
+char *read_file (const char *, const char *);
 char *encrypt_token (const char *, char *);
-char *decrypt_skey (const char *, const char *);
+char *decrypt_token (const char *, const char *);
+void create_enc_file();
+char *b64_encode (unsigned char *, size_t);
+struct _b64OutData *b64_decode (const char *);
+void multi_free (void *, void *, void *);
 
 #endif
