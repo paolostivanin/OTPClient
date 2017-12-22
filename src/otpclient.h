24645d87e955cfdffac3e54d96dac416d1809be2
Paolo Stivanin <info@paolostivanin.com>
2017-12-22 14:44:03 +0100

    v1.0.2
    - decrease secure memory pool to 256 KiB
    - move appdata to root folder
    - clean up flatpak json
    - check mlockall return code

--- src/otpclient.h
+++ src/otpclient.h
@@ -6,10 +6,10 @@
 G_BEGIN_DECLS
 
 #define APP_NAME                "OTPClient"
-#define APP_VERSION             "1.0.1"
+#define APP_VERSION             "1.0.2"
 
 #define HOTP_RATE_LIMIT_IN_SEC  3
-#define MAX_FILE_SIZE           1048576  // 1 MiB should be more than enough for such content.
+#define MAX_FILE_SIZE           262144  // 256 KiB should be more than enough for such content.
 
 void activate               (GtkApplication *app,
                              gpointer user_data);
