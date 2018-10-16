368e7ceea9d64c251505443f665994c151bd4da0
Paolo Stivanin <info@paolostivanin.com>
2018-10-16 17:02:00 +0200

    Fix crash when pwd mismatch

--- src/password-cb.c
+++ src/password-cb.c
@@ -14,6 +14,9 @@ typedef struct _entrywidgets {
     gchar *cur_pwd;
 } EntryWidgets;
 
+static void send_ok_cb    (GtkWidget *entry,
+                           gpointer   user_data);
+
 static void check_pwd_cb  (GtkWidget *entry,
                            gpointer   user_data);
 
@@ -38,14 +41,14 @@ prompt_for_password (AppData *app_data, gchar *current_key)
         gtk_label_set_text (GTK_LABEL(gtk_builder_get_object (builder, "decpwd_label_id")), text);
         g_free (text);
         entry_widgets->entry1 = GTK_WIDGET(gtk_builder_get_object (builder,"decpwddiag_entry_id"));
-        g_signal_connect (entry_widgets->entry1, "activate", G_CALLBACK (password_cb), (gpointer *) &entry_widgets->pwd);
+        g_signal_connect (entry_widgets->entry1, "activate", G_CALLBACK (send_ok_cb), NULL);
         g_signal_connect (entry_widgets->entry1, "icon-press", G_CALLBACK (icon_press_cb), NULL);
     } else if (file_exists == FALSE && current_key == NULL) {
         // new db dialog, 2 fields
         dialog = GTK_WIDGET(gtk_builder_get_object (builder, "newdb_pwd_diag_id"));
         entry_widgets->entry1 = GTK_WIDGET(gtk_builder_get_object (builder,"newdb_pwd_diag_entry1_id"));
         entry_widgets->entry2 = GTK_WIDGET(gtk_builder_get_object (builder,"newdb_pwd_diag_entry2_id"));
-        g_signal_connect (entry_widgets->entry2, "activate", G_CALLBACK (check_pwd_cb), entry_widgets);
+        g_signal_connect (entry_widgets->entry2, "activate", G_CALLBACK (send_ok_cb), NULL);
         g_signal_connect (entry_widgets->entry1, "icon-press", G_CALLBACK (icon_press_cb), NULL);
         g_signal_connect (entry_widgets->entry2, "icon-press", G_CALLBACK (icon_press_cb), NULL);
     } else {
@@ -55,7 +58,7 @@ prompt_for_password (AppData *app_data, gchar *current_key)
         entry_widgets->entry_old = GTK_WIDGET(gtk_builder_get_object (builder,"changepwd_diag_currententry_id"));
         entry_widgets->entry1 = GTK_WIDGET(gtk_builder_get_object (builder,"changepwd_diag_newentry1_id"));
         entry_widgets->entry2 = GTK_WIDGET(gtk_builder_get_object (builder,"changepwd_diag_newentry2_id"));
-        g_signal_connect (entry_widgets->entry2, "activate", G_CALLBACK (check_pwd_cb), entry_widgets);
+        g_signal_connect (entry_widgets->entry2, "activate", G_CALLBACK (send_ok_cb), NULL);
         g_signal_connect (entry_widgets->entry1, "icon-press", G_CALLBACK (icon_press_cb), NULL);
         g_signal_connect (entry_widgets->entry2, "icon-press", G_CALLBACK (icon_press_cb), NULL);
         g_signal_connect (entry_widgets->entry_old, "icon-press", G_CALLBACK (icon_press_cb), NULL);
@@ -67,15 +70,15 @@ prompt_for_password (AppData *app_data, gchar *current_key)
 
     gint ret;
     do {
-        ret = gtk_dialog_run (GTK_DIALOG (dialog));
+        ret = gtk_dialog_run (GTK_DIALOG(dialog));
         if (ret == GTK_RESPONSE_OK) {
             if (file_exists) {
-                password_cb (entry_widgets->entry1, (gpointer *) &entry_widgets->pwd);
+                password_cb (entry_widgets->entry1, (gpointer *)&entry_widgets->pwd);
             } else {
-                check_pwd_cb (entry_widgets->entry1, (gpointer) entry_widgets);
+                check_pwd_cb (entry_widgets->entry1, (gpointer)entry_widgets);
             }
         }
-    } while (ret == GTK_RESPONSE_ACCEPT && entry_widgets->retry == TRUE);
+    } while (ret == GTK_RESPONSE_OK && entry_widgets->retry == TRUE);
 
     gchar *pwd = NULL;
     if (entry_widgets->pwd != NULL) {
@@ -99,22 +102,30 @@ prompt_for_password (AppData *app_data, gchar *current_key)
 
 
 static void
+send_ok_cb (GtkWidget *entry,
+            gpointer   user_data __attribute__((unused)))
+{
+    gtk_dialog_response (GTK_DIALOG (gtk_widget_get_toplevel (entry)), GTK_RESPONSE_OK);
+}
+
+
+static void
 check_pwd_cb (GtkWidget   *entry,
               gpointer     user_data)
 {
     EntryWidgets *entry_widgets = (EntryWidgets *) user_data;
-    if (entry_widgets->cur_pwd != NULL && g_strcmp0 (gtk_entry_get_text (GTK_ENTRY (entry_widgets->entry_old)), entry_widgets->cur_pwd) != 0) {
+    if (entry_widgets->cur_pwd != NULL && g_strcmp0 (gtk_entry_get_text (GTK_ENTRY(entry_widgets->entry_old)), entry_widgets->cur_pwd) != 0) {
         show_message_dialog (gtk_widget_get_toplevel (entry), "Old password doesn't match", GTK_MESSAGE_ERROR);
         entry_widgets->retry = TRUE;
         return;
     }
-    if (gtk_entry_get_text_length (GTK_ENTRY (entry_widgets->entry1)) < 6) {
+    if (gtk_entry_get_text_length (GTK_ENTRY(entry_widgets->entry1)) < 6) {
         show_message_dialog (gtk_widget_get_toplevel (entry), "Password must be at least 6 characters.", GTK_MESSAGE_ERROR);
         entry_widgets->retry = TRUE;
         return;
     }
-    if (g_strcmp0 (gtk_entry_get_text (GTK_ENTRY (entry_widgets->entry1)), gtk_entry_get_text (GTK_ENTRY (entry_widgets->entry2))) == 0) {
-        password_cb (entry, (gpointer *) &entry_widgets->pwd);
+    if (g_strcmp0 (gtk_entry_get_text (GTK_ENTRY(entry_widgets->entry1)), gtk_entry_get_text (GTK_ENTRY(entry_widgets->entry2))) == 0) {
+        password_cb (entry, (gpointer *)&entry_widgets->pwd);
         entry_widgets->retry = FALSE;
     } else {
         show_message_dialog (gtk_widget_get_toplevel (entry), "Passwords mismatch", GTK_MESSAGE_ERROR);
