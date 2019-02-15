6b42c060a466136046cef49b680ad8e5c2dc5e04
Paolo Stivanin <info@paolostivanin.com>
2019-02-15 15:19:29 +0100

    Use monospace for database path
    - fix #116

--- src/password-cb.c
+++ src/password-cb.c
@@ -38,18 +38,24 @@ prompt_for_password (AppData        *app_data,
 
     gboolean pwd_must_be_checked = TRUE;
     gboolean file_exists = g_file_test (app_data->db_data->db_path, G_FILE_TEST_EXISTS);
-    if ((file_exists == TRUE || action_name != NULL) && current_key == NULL) {
+    if ((file_exists == TRUE || action_name != NULL) && current_key == NULL && is_export_pwd == FALSE) {
         // decrypt dialog, just one field
         pwd_must_be_checked = FALSE;
         dialog = GTK_WIDGET(gtk_builder_get_object (builder, "decpwd_diag_id"));
-        gchar *text;
+        gchar *text = NULL, *markup = NULL;
         if (action_name == NULL){
-            text = g_strconcat ("Enter the decryption password for ", app_data->db_data->db_path, NULL);
+            markup = g_markup_printf_escaped ("%s <span font_family=\"monospace\">%s</span>", "Enter the decryption password for ", app_data->db_data->db_path);
         } else {
             text = g_strdup ("Enter the decryption password");
         }
-        gtk_label_set_text (GTK_LABEL(gtk_builder_get_object (builder, "decpwd_label_id")), text);
-        g_free (text);
+        GtkLabel *label = GTK_LABEL(gtk_builder_get_object (builder, "decpwd_label_id"));
+        if (markup != NULL) {
+            gtk_label_set_markup (label, markup);
+            g_free (markup);
+        } else {
+            gtk_label_set_text (label, text);
+            g_free (text);
+        }
         entry_widgets->entry1 = GTK_WIDGET(gtk_builder_get_object (builder,"decpwddiag_entry_id"));
         g_signal_connect (entry_widgets->entry1, "activate", G_CALLBACK (send_ok_cb), NULL);
         g_signal_connect (entry_widgets->entry1, "icon-press", G_CALLBACK (icon_press_cb), NULL);
