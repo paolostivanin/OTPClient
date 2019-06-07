ba5305b2be6b6d571ab615d16a2487dde3be0a70
Paolo Stivanin <info@paolostivanin.com>
2019-06-07 11:35:01 +0200

    Fix andOTP import and export
    - correctly parse label when importing data
    - correctly create label when exporting data

--- src/andotp.c
+++ src/andotp.c
@@ -124,7 +124,7 @@ export_andotp ( const gchar *export_path,
 
         // FIXME: issuer might be empty
         gchar *constructed_label = g_strconcat (json_string_value (json_object_get (db_obj, "issuer")),
-                                                " - ",
+                                                ":",
                                                 json_string_value (json_object_get (db_obj, "label")),
                                                 NULL);
         json_object_set (export_obj, "label", json_string (constructed_label));
@@ -229,7 +229,7 @@ parse_json_data (const gchar *data,
         otp->secret = secure_strdup (json_string_value (json_object_get (obj, "secret")));
 
         const gchar *account_with_issuer = json_string_value (json_object_get (obj, "label"));
-        gchar **tokens = g_strsplit (account_with_issuer, "-", -1);
+        gchar **tokens = g_strsplit (account_with_issuer, ":", -1);
         if (tokens[0] && tokens[1]) {
             otp->issuer = g_strdup (g_strstrip (tokens[0]));
             otp->account_name = g_strdup (g_strstrip (tokens[1]));
