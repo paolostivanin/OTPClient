688ef2280ad335c2717f57b474335971d829c3c3
Paolo Stivanin <info@paolostivanin.com>
2019-12-18 15:57:46 +0100

    Small fixes to message dialog code

--- src/message-dialogs.c
+++ src/message-dialogs.c
@@ -7,15 +7,10 @@ show_message_dialog (GtkWidget      *parent,
 {
     static GtkWidget *dialog = NULL;
 
-    if (parent == NULL) {
-        dialog = gtk_message_dialog_new (NULL, GTK_DIALOG_MODAL, message_type, GTK_BUTTONS_OK, NULL);
-    } else {
-        dialog = gtk_message_dialog_new (GTK_WINDOW (parent), GTK_DIALOG_MODAL, message_type, GTK_BUTTONS_OK, NULL);
-    }
+    dialog = gtk_message_dialog_new (parent == NULL ? NULL : GTK_WINDOW(parent), GTK_DIALOG_MODAL, message_type, GTK_BUTTONS_OK, "%s", message);
+    gtk_message_dialog_set_markup (GTK_MESSAGE_DIALOG(dialog), message);
 
-    gtk_message_dialog_set_markup (GTK_MESSAGE_DIALOG (dialog), message);
-
-    gtk_dialog_run (GTK_DIALOG (dialog));
+    gtk_dialog_run (GTK_DIALOG(dialog));
 
     gtk_widget_destroy (dialog);
 }
