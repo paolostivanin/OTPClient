2671dc1f50e8bdb20c1908f7fc94fdd78869d950
Paolo Stivanin <info@paolostivanin.com>
2023-05-25 16:51:25 +0200

    Add new Database info dialog

---
+++ src/dbinfo-cb.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+void dbinfo_cb     (GSimpleAction *simple,
+                    GVariant      *parameter,
+                    gpointer       user_data);
+
+G_END_DECLS
